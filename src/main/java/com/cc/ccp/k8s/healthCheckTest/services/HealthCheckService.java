42efd6b2a9b22b059c4f4007092c6892de8980cc
Tamas Dober <tamasdober@gmail.com>
2020-10-25 23:19:19 -0700

    adding new method to the service

--- src/main/java/com/cc/ccp/k8s/healthCheckTest/services/HealthCheckService.java
+++ src/main/java/com/cc/ccp/k8s/healthCheckTest/services/HealthCheckService.java
@@ -2,6 +2,7 @@ package com.cc.ccp.k8s.healthCheckTest.services;
 
 import com.cc.ccp.k8s.healthCheckTest.command.CreateDeploymentCommand;
 import com.cc.ccp.k8s.healthCheckTest.command.KubernetesActionExecutor;
+import com.cc.ccp.k8s.healthCheckTest.command.ScaleDownDeploymentCommand;
 import com.cc.ccp.k8s.healthCheckTest.model.KubernetesObjectToDisable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class HealthCheckService {
 
-    public boolean createSendCommand(KubernetesObjectToDisable kubernetesObjectToDisable) {
-        log.info("service received: " + kubernetesObjectToDisable.getName());
+    public boolean createDeploymentSendCommand(KubernetesObjectToDisable kubernetesObjectToDisable) {
+        log.info("service received: " + kubernetesObjectToDisable.getDeploymentName());
         KubernetesActionExecutor kubernetesActionExecutor = new KubernetesActionExecutor();
-        kubernetesActionExecutor.executeOperation(new CreateDeploymentCommand(),kubernetesObjectToDisable.getName(),null);
+        kubernetesActionExecutor.executeOperation(new CreateDeploymentCommand(), kubernetesObjectToDisable.getDeploymentName(), kubernetesObjectToDisable.getNamespace());
         return true;
-
     }
+
+    public boolean scaleDownDeploymentCommand(KubernetesObjectToDisable kubernetesObjectToDisable) {
+        log.info("service received: " + kubernetesObjectToDisable.getDeploymentName());
+        KubernetesActionExecutor kubernetesActionExecutor = new KubernetesActionExecutor();
+        kubernetesActionExecutor.executeOperation(new ScaleDownDeploymentCommand(), kubernetesObjectToDisable.getDeploymentName(), kubernetesObjectToDisable.getNamespace());
+        return true;
+    }
+
 }
