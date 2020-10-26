5307718f8d02b2abfd03fe2aa8e98470b1454f55
Tamas Dober <tamasdober@gmail.com>
2020-10-25 23:14:22 -0700

    adding new field to the model; renaming exisitng field

--- src/main/java/com/cc/ccp/k8s/healthCheckTest/model/KubernetesObjectToDisable.java
+++ src/main/java/com/cc/ccp/k8s/healthCheckTest/model/KubernetesObjectToDisable.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class KubernetesObjectToDisable {
 
-    private final String name;
+    private final String deploymentName;
+    private final String namespace;
 
-    public KubernetesObjectToDisable(@JsonProperty("name") String name) {
-        this.name = name;
+    public KubernetesObjectToDisable(@JsonProperty("deploymentName") String deploymentName, @JsonProperty("namespace") String namespace) {
+        this.deploymentName = deploymentName;
+        this.namespace = namespace;
     }
 
-    public String getName() {
-        return name;
+    public String getDeploymentName() {
+        return deploymentName;
+    }
+
+    public String getNamespace() {
+        return namespace;
     }
 
 }
