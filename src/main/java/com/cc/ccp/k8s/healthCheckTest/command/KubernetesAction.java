d3c47001612411db89cc287805e2c5ba995db267
Tamas Dober <tamasdober@gmail.com>
2020-10-25 23:16:08 -0700

    updating interface and executor with the new field

--- src/main/java/com/cc/ccp/k8s/healthCheckTest/command/KubernetesAction.java
+++ src/main/java/com/cc/ccp/k8s/healthCheckTest/command/KubernetesAction.java
@@ -2,6 +2,6 @@ package com.cc.ccp.k8s.healthCheckTest.command;
 
 public interface KubernetesAction {
 
-    void execute(String name, String location);
+    void execute(String deploymentName, String namespace);
 
 }
--- src/main/java/com/cc/ccp/k8s/healthCheckTest/command/KubernetesActionExecutor.java
+++ src/main/java/com/cc/ccp/k8s/healthCheckTest/command/KubernetesActionExecutor.java
@@ -5,8 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class KubernetesActionExecutor {
 
-    public void executeOperation(KubernetesAction kubernetesAction, String name, String location) {
+    public void executeOperation(KubernetesAction kubernetesAction, String deploymentName, String namespace) {
         log.info("executing kubernetesAction ");
-        kubernetesAction.execute(name, location);
+        kubernetesAction.execute(deploymentName, namespace);
     }
 }
