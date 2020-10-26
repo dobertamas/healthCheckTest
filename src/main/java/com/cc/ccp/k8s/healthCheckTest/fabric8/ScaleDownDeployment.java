9ae889b575fc5d55b0d2184a57a29834012ba668
Tamas Dober <tamasdober@gmail.com>
2020-10-25 23:17:44 -0700

    adding new fabric8 class for scaling down deployment

---
+++ src/main/java/com/cc/ccp/k8s/healthCheckTest/fabric8/ScaleDownDeployment.java
@@ -0,0 +1,28 @@
+package com.cc.ccp.k8s.healthCheckTest.fabric8;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ScaleDownDeployment {
+
+    public void scaleDownDefaultNamespace(String deployment) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+
+            client.apps().deployments().inNamespace("default").withName(deployment).scale(0, false);
+
+        }
+    }
+
+    public void scaleDownWithNamespace(String deployment, String namespace) {
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            log.info("namespace: " + namespace);
+            log.info("deployment: " + deployment);
+            client.apps().deployments().inNamespace(namespace).withName(deployment).scale(0, false);
+
+        }
+    }
+
+
+}
